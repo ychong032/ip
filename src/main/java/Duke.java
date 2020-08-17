fd3ad6912ec7f88f9ec3b5672d8da962d3743d75
ychong032 <yowlimchong2403@gmail.com>
2020-08-17 16:34:19 +0800

    Add skeletal message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        
+        System.out.println("-------------------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------");
     }
 }

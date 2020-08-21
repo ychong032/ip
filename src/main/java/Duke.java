861c9eea3048c6cba5c752a81d6b3affd326d8e2
ychong032 <yowlimchong2403@gmail.com>
2020-08-21 15:08:21 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("-------------------------------");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("-------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------");
+        printDivider();
+        System.out.printf("\tHello! I'm Duke\n");
+        System.out.printf("\tWhat can I do for you?\n");
+        printDivider();
+        loopEcho();
+    }
+
+    public static void printDivider() {
+        System.out.printf("\t-------------------------------------\n");
+    }
+
+    public static void loopEcho() {
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        while (!message.equals("bye")) {
+            printDivider();
+            System.out.printf("\t%s%n", message);
+            printDivider();
+            message = sc.nextLine();
+        }
+        printDivider();
+        System.out.printf("\tBye. Hope to see you again soon!\n");
+        printDivider();
     }
 }

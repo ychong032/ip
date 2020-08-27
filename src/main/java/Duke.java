221c70c8d87225b404660b0093bc37bd5a086ef1
ychong032 <yowlimchong2403@gmail.com>
2020-08-27 17:16:41 +0800

    Change variable names and add logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,12 @@ public class Duke {
     static int taskCount = 0;
 
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         //Opening message
         printDivider();
         System.out.println("\tGreetings, sir. My name is Duke");
@@ -23,52 +29,52 @@ public class Duke {
 
     public static void loopEcho() {
         Scanner sc = new Scanner(System.in);
-        String message = sc.nextLine();
-        while (!message.equals("bye")) {
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
             printDivider();
-            System.out.printf("\t%s%n", message);
+            System.out.printf("\t%s%n", userInput);
             printDivider();
-            message = sc.nextLine();
+            userInput = sc.nextLine();
         }
     }
 
     public static void addToList() {
-        Task[] list =  new Task[100];
+        Task[] taskList =  new Task[100];
         int listIndex = 0;
         Scanner sc = new Scanner(System.in);
-        String message = sc.nextLine();
-        while (!message.equals("bye")) {
-            if (message.equals("list")) {
-                displayList(list);
-                message = sc.nextLine();
+        String userInput = sc.nextLine();
+        while (!userInput.trim().toLowerCase().equals("bye")) {
+            if (userInput.trim().toLowerCase().equals("list")) {
+                displayList(taskList);
+                userInput = sc.nextLine();
                 continue;
             }
-            if (message.contains("done")) {
-                int taskNumber = Integer.parseInt(message.substring(5));
-                Task taskDone = list[taskNumber-1];
+            if (userInput.trim().toLowerCase().contains("done")) {
+                int taskNumber = Integer.parseInt(userInput.substring(5));
+                Task taskDone = taskList[taskNumber-1];
                 taskDone.markAsDone();
                 printDivider();
                 System.out.println("\tVery good, sir. Excellent work accomplishing your task:");
                 System.out.printf("\t[%s] %s%n", taskDone.getStatusIcon(), taskDone.description);
                 printDivider();
-                message = sc.nextLine();
+                userInput = sc.nextLine();
                 continue;
             }
-            list[listIndex] = new Task(message);
+            taskList[listIndex] = new Task(userInput);
             printDivider();
-            System.out.printf("\tadded: %s%n", list[listIndex].description);
+            System.out.printf("\tadded: %s%n", taskList[listIndex].description);
             printDivider();
             listIndex++;
             taskCount++;
-            message = sc.nextLine();
+            userInput = sc.nextLine();
         }
     }
 
-    public static void displayList(Task[] list) {
+    public static void displayList(Task[] taskList) {
         printDivider();
         System.out.println("\tHere are your tasks for this evening, sir: ");
         for (int i = 0; i < taskCount; i++) {
-            System.out.printf("\t%d.[%s] %s", i+1, list[i].getStatusIcon(), list[i].description);
+            System.out.printf("\t%d.[%s] %s", i+1, taskList[i].getStatusIcon(), taskList[i].description);
             System.out.print('\n');
         }
         printDivider();

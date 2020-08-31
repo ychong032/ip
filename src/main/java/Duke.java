024e742b00097d1780bd3b9a1e3913c9891abe83
ychong032 <yowlimchong2403@gmail.com>
2020-08-31 15:55:12 +0800

    Add ToDo, Deadline, Event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,8 @@ public class Duke {
         printGreeting();
         printDivider();
 
-        addToList();
+        //Receive user input and execute corresponding functions
+        getUserInput();
 
         //Print closing message
         printDivider();
@@ -37,7 +38,7 @@ public class Duke {
     }
 
     public static void printDivider() {
-        System.out.println("\t+--------------------------------------------------------+");
+        System.out.println("\t+------------------------------------------------------------------+");
     }
 
 
@@ -52,7 +53,7 @@ public class Duke {
         }
     }
 
-    public static void addToList() {
+    public static void getUserInput() {
         Task[] taskList =  new Task[MAX_TASK_LENGTH];
         int listIndex = 0;
         Scanner sc = new Scanner(System.in);
@@ -64,35 +65,74 @@ public class Duke {
                 continue;
             }
             if (userInput.trim().toLowerCase().contains("done")) {
-                int taskNumber = Integer.parseInt(userInput.substring(5)); //Magic number!
+                String replacedInput = userInput.trim().toLowerCase().replace("done", "");
+                int taskNumber = Integer.parseInt(replacedInput.trim().substring(0, 1));
                 Task taskDone = taskList[taskNumber-1];
                 taskDone.markAsDone();
                 printDivider();
                 System.out.println("\tVery good, sir. Excellent work accomplishing your task:");
-                System.out.printf("\t[%s] %s%n", taskDone.getStatusIcon(), taskDone.description);
+                System.out.print('\t');
+                System.out.println(taskDone);
                 printDivider();
-                userInput = sc.nextLine();
-                continue;
+            } else if (userInput.trim().toLowerCase().contains("todo")) {
+                listIndex = addToList(taskList, listIndex, userInput, "todo");
+            } else if (userInput.trim().toLowerCase().contains("deadline")) {
+                listIndex = addToList(taskList, listIndex, userInput, "deadline");
+            } else if (userInput.trim().toLowerCase().contains("event")) {
+                listIndex = addToList(taskList, listIndex, userInput, "event");
+            } else {
+                printDivider();
+                System.out.println("\tI'm afraid I don't understand, sir. Please preface your tasks " +
+                        "\n\twith 'todo', 'deadline', or 'event' to add them to your list.");
+                printDivider();
             }
-            taskList[listIndex] = new Task(userInput);
-            printDivider();
-            System.out.printf("\tadded: %s%n", taskList[listIndex].description);
-            printDivider();
-            listIndex++;
-            taskCount++;
             userInput = sc.nextLine();
         }
+    }
+
+    public static int addToList(Task[] taskList, int listIndex, String userInput, String taskType) {
+        String replacedInput;
+        String description;
+        switch (taskType) {
+        case "todo":
+            replacedInput = userInput.replaceFirst("(?i)todo", "").trim();
+            description = replacedInput;
+            taskList[listIndex] = new ToDo(description);
+            break;
+        case "deadline":
+            replacedInput = userInput.replaceFirst("(?i)deadline", "").trim();
+            int byIndex = replacedInput.toLowerCase().indexOf("/by");
+            String by = replacedInput.substring(byIndex + 3);
+            description = replacedInput.substring(0, byIndex);
+            taskList[listIndex] = new Deadline(description, by);
+            break;
+        case "event":
+            replacedInput = userInput.replaceFirst("(?i)event", "").trim();
+            int atIndex = replacedInput.indexOf("/at");
+            String at = replacedInput.substring(atIndex + 3);
+            description = replacedInput.substring(0, atIndex);
+            taskList[listIndex] = new Event(description, at);
+            break;
+        }
+        printDivider();
+        System.out.println("\tUnderstood, sir. I've added this task: ");
+        System.out.println("\t\t" + taskList[listIndex]);
+        listIndex++;
+        taskCount++;
+        System.out.printf("\tYou currently have %d task(s) in your list, sir.%n", taskCount);
+        printDivider();
+        return listIndex;
     }
 
     public static void displayList(Task[] taskList) {
         printDivider();
         if (taskList[0] == null) {
-            System.out.println("\tYou have no tasks recorded this evening, sir.");
+            System.out.println("\tYou have no tasks recorded, sir. Huzzah!");
         } else {
-            System.out.println("\tHere are your tasks for this evening, sir: ");
+            System.out.println("\tHere are your tasks, sir: ");
             for (int i = 0; i < taskCount; i++) {
-                System.out.printf("\t%d.[%s] %s", i + 1, taskList[i].getStatusIcon(), taskList[i].description);
-                System.out.print('\n');
+                System.out.printf("\t%d.", i + 1);
+                System.out.println(taskList[i]);
             }
         }
         printDivider();

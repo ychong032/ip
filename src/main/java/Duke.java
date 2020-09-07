ce427f32897d833681ebb8d8039912e80ef10658
ychong032 <yowlimchong2403@gmail.com>
2020-09-07 16:09:28 +0800

    Improve code quality, handle basic input errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Duke {
     private static final int MAX_TASK_LENGTH = 100;
     private static int taskCount = 0;
+    private static int listIndex = 0;
 
     public static void main(String[] args) {
         //Print logo and opening message
@@ -15,20 +16,8 @@ public class Duke {
         printFarewell();
     }
 
-    public static void loopEcho() {
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        while (!userInput.equals("bye")) {
-            printDivider();
-            System.out.printf("\t%s%n", userInput);
-            printDivider();
-            userInput = sc.nextLine();
-        }
-    }
-
     public static void getUserInput() {
         Task[] taskList = new Task[MAX_TASK_LENGTH];
-        int listIndex = 0;
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.trim().toLowerCase().equals("bye")) {
@@ -37,11 +26,11 @@ public class Duke {
             } else if (userInput.trim().toLowerCase().contains("done")) {
                 markTaskDone(taskList, userInput);
             } else if (userInput.trim().toLowerCase().contains("todo")) {
-                listIndex = addToList(taskList, listIndex, userInput, "todo");
+                addToList(taskList, userInput, "todo");
             } else if (userInput.trim().toLowerCase().contains("deadline")) {
-                listIndex = addToList(taskList, listIndex, userInput, "deadline");
+                addToList(taskList, userInput, "deadline");
             } else if (userInput.trim().toLowerCase().contains("event")) {
-                listIndex = addToList(taskList, listIndex, userInput, "event");
+                addToList(taskList, userInput, "event");
             } else {
                 printErrorMessage();
             }
@@ -53,7 +42,8 @@ public class Duke {
         Task taskDone = getCompletedTask(taskList, userInput);
         taskDone.markAsDone();
         printDivider();
-        System.out.println("\tVery good, sir. Excellent work accomplishing your task:");
+        String clapEmoji = new String(Character.toChars(0x1f44f));
+        System.out.println("\t" + clapEmoji + " Very good, sir. Excellent work accomplishing your task:");
         System.out.print('\t');
         System.out.println(taskDone);
         printDivider();
@@ -65,44 +55,76 @@ public class Duke {
         return taskList[taskNumber-1];
     }
 
-    public static int addToList(Task[] taskList, int listIndex, String userInput, String taskType) {
-        String replacedInput;
-        String description;
-        switch (taskType) {
-        case "todo":
-            replacedInput = userInput.replaceFirst("(?i)todo", "").trim();
-            description = replacedInput;
-            taskList[listIndex] = new ToDo(description);
-            break;
-        case "deadline":
-            replacedInput = userInput.replaceFirst("(?i)deadline", "").trim();
-            int byIndex = replacedInput.toLowerCase().indexOf("/by");
-            String by = replacedInput.substring(byIndex + 3);
-            description = replacedInput.substring(0, byIndex);
-            taskList[listIndex] = new Deadline(description, by);
-            break;
-        case "event":
-            replacedInput = userInput.replaceFirst("(?i)event", "").trim();
-            int atIndex = replacedInput.indexOf("/at");
-            String at = replacedInput.substring(atIndex + 3);
-            description = replacedInput.substring(0, atIndex);
-            taskList[listIndex] = new Event(description, at);
-            break;
+    public static void addToList(Task[] taskList, String userInput, String taskType) {
+        String replacedInput = userInput.replaceFirst("(?i)" + taskType, "").trim();
+        if (isValidInput(replacedInput, taskType)) {
+            switch (taskType) {
+            case "todo":
+                addToDoToList(taskList, replacedInput);
+                break;
+            case "deadline":
+                addDeadlineToList(taskList, replacedInput);
+                break;
+            case "event":
+                addEventToList(taskList, replacedInput);
+                break;
+            }
         }
+    }
+
+    public static void printAddedTask(Task[] taskList) {
         printDivider();
-        System.out.println("\tUnderstood, sir. I've added this task: ");
+        String okayEmoji = new String(Character.toChars(0x1f44c));
+        System.out.println("\t" + okayEmoji + " Understood, sir. I've added this task: ");
         System.out.println("\t\t" + taskList[listIndex]);
-        listIndex++;
-        taskCount++;
         System.out.printf("\tYou currently have %d task(s) in your list, sir.%n", taskCount);
         printDivider();
-        return listIndex;
+    }
+
+    public static void addToDoToList(Task[] taskList, String replacedInput) {
+        taskList[listIndex] = new ToDo(replacedInput);
+        taskCount++;
+        printAddedTask(taskList);
+        listIndex++;
+    }
+
+    public static void addDeadlineToList(Task[] taskList, String replacedInput) {
+        int byIndex = replacedInput.toLowerCase().indexOf("/by");
+        String by = replacedInput.substring(byIndex + 3);
+        String description = replacedInput.substring(0, byIndex);
+        taskList[listIndex] = new Deadline(description, by);
+        taskCount++;
+        printAddedTask(taskList);
+        listIndex++;
+    }
+
+    public static void addEventToList(Task[] taskList, String replacedInput) {
+        int atIndex = replacedInput.toLowerCase().indexOf("/at");
+        String at = replacedInput.substring(atIndex + 3);
+        String description = replacedInput.substring(0, atIndex);
+        taskList[listIndex] = new Event(description, at);
+        taskCount++;
+        printAddedTask(taskList);
+        listIndex++;
+    }
+
+    public static boolean isValidInput(String replacedInput, String taskType) {
+        if (replacedInput.isBlank()) {
+            printDivider();
+            String sadEmoji = new String(Character.toChars(0x1f61e));
+            System.out.println("\t" + sadEmoji + " Apologies, sir. The description of your " + taskType
+                    + " cannot be empty.");
+            printDivider();
+            return false;
+        }
+        return true;
     }
 
     public static void displayList(Task[] taskList) {
         printDivider();
         if (taskList[0] == null) {
-            System.out.println("\tYou have no tasks recorded, sir. Huzzah!");
+            String hurrayEmoji = new String(Character.toChars(0x1f64c));
+            System.out.println("\t" + hurrayEmoji + " You have no tasks recorded, sir. Huzzah!");
         } else {
             System.out.println("\tHere are your tasks, sir: ");
             for (int i = 0; i < taskCount; i++) {
@@ -115,22 +137,24 @@ public class Duke {
 
     public static void printFarewell() {
         printDivider();
-        System.out.println("\tAlways a pleasure, sir. Do come back soon.");
+        String smileEmoji = new String(Character.toChars(0x1f600));
+        System.out.println("\t" + smileEmoji + " Always a pleasure, sir. Do come back soon.");
         printDivider();
     }
 
     public static void printLogo() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("\tHello from\n" + logo);
     }
 
     public static void printGreeting() {
         printDivider();
-        System.out.println("\tGreetings, sir. My name is Duke");
+        String waveEmoji = new String(Character.toChars(0x1f64b));
+        System.out.println("\t" + waveEmoji + " Greetings, sir. My name is Duke");
         System.out.println("\tHow may I assist you today, sir?");
         printDivider();
     }
@@ -141,8 +165,9 @@ public class Duke {
 
     public static void printErrorMessage() {
         printDivider();
-        System.out.println("\tI'm afraid I don't understand, sir. Please preface your tasks " +
-                "\n\twith 'todo', 'deadline', or 'event' to add them to your list.");
+        String confusedEmoji = new String(Character.toChars(0x1f615));
+        System.out.println("\t" + confusedEmoji + " I'm afraid I don't understand, sir. Please preface " +
+                "your tasks " + "\n\twith 'todo', 'deadline', or 'event' to add them to your list.");
         printDivider();
     }
 }

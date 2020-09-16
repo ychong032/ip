72c1da2adf762e05e41d8a0c9f6ac04d1f81974f
ychong032 <yowlimchong2403@gmail.com>
2020-09-16 15:32:19 +0800

    Add Delete function, clean up code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,11 +5,11 @@ import duke.task.Deadline;
 import duke.task.ToDo;
 import duke.task.Event;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    private static final int MAX_TASK_LENGTH = 100;
-    private static int taskCount = 0;
     private static int listIndex = 0;
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         //Print logo and opening message
@@ -24,20 +24,21 @@ public class Duke {
     }
 
     public static void getUserInput() {
-        Task[] taskList = new Task[MAX_TASK_LENGTH];
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.trim().toLowerCase().equals("bye")) {
             if (userInput.trim().toLowerCase().equals("list")) {
-                displayList(taskList);
+                displayList();
             } else if (userInput.trim().toLowerCase().contains("done")) {
-                markTaskDone(taskList, userInput);
+                markTaskDone(userInput);
             } else if (userInput.trim().toLowerCase().contains("todo")) {
-                addToList(taskList, userInput, "todo");
+                addToList(userInput, "todo");
             } else if (userInput.trim().toLowerCase().contains("deadline")) {
-                addToList(taskList, userInput, "deadline");
+                addToList(userInput, "deadline");
             } else if (userInput.trim().toLowerCase().contains("event")) {
-                addToList(taskList, userInput, "event");
+                addToList(userInput, "event");
+            } else if (userInput.trim().toLowerCase().contains("delete")) {
+                deleteTask(userInput);
             } else {
                 printErrorMessage();
             }
@@ -45,59 +46,83 @@ public class Duke {
         }
     }
 
-    public static void markTaskDone(Task[] taskList, String userInput) {
+    public static void markTaskDone(String userInput) {
         try {
-            Task taskDone = getCompletedTask(taskList, userInput);
+            Task taskDone = getCompletedTask(userInput);
             taskDone.markAsDone();
             printTaskDone(taskDone);
         } catch (NumberFormatException e) {
             printNFEMessage();
-        } catch (NullPointerException e) {
-            printNPEMessage();
+        } catch (IndexOutOfBoundsException e) {
+            printIOOEMessage();
         }
     }
 
-    public static void printNPEMessage() {
+    public static void deleteTask(String userInput) {
+        try {
+            Task deletedTask = getDeletedTask(userInput);
+            printTaskDeleted(deletedTask);
+            tasks.remove(deletedTask);
+        } catch (NumberFormatException e) {
+            printNFEMessage();
+        } catch (IndexOutOfBoundsException e) {
+            printIOOEMessage();
+        }
+    }
+
+    public static void printTaskDeleted(Task deletedTask) {
+        printDivider();
+        System.out.println("\tUnderstood, I've deleted this task: ");
+        System.out.println("\t\t" + deletedTask);
+        System.out.printf("\tYou now have %d task(s) in your list, sir.\n", tasks.size() - 1);
+        printDivider();
+    }
+
+    public static Task getDeletedTask(String userInput) throws NumberFormatException, IndexOutOfBoundsException {
+        String replacedInput = userInput.trim().toLowerCase().replace("delete", "");
+        int taskDeleted = Integer.parseInt(replacedInput.trim());
+        return tasks.get(taskDeleted - 1);
+    }
+
+    public static void printIOOEMessage() {
         printDivider();
         System.out.println("\t" + SAD_EMOJI + " That task number is not in your list, sir. Please select an"
-                + "\n\texisting task number to mark it as completed.");
+                + "\n\texisting task number.");
         printDivider();
     }
 
     public static void printNFEMessage() {
         printDivider();
-        System.out.println("\t" + SAD_EMOJI + " I don't recognise that task number, sir. Please enter" +
-                "\n\ta valid task number after the 'done'.");
+        System.out.println("\t" + SAD_EMOJI + " I don't recognise that task number, sir. Please enter a valid" +
+                "\n\ttask number after the keyword.");
         printDivider();
     }
 
     public static void printTaskDone(Task taskDone) {
         printDivider();
         System.out.println("\t" + CLAP_EMOJI + " Very good, sir. Excellent work accomplishing your task:");
-        System.out.print('\t');
-        System.out.println(taskDone);
+        System.out.println("\t\t" + taskDone);
         printDivider();
     }
 
-    public static Task getCompletedTask(Task[] taskList, String userInput) throws NumberFormatException,
-            NullPointerException {
+    public static Task getCompletedTask(String userInput) throws NumberFormatException, IndexOutOfBoundsException {
         String replacedInput = userInput.trim().toLowerCase().replace("done", "");
         int taskNumber = Integer.parseInt(replacedInput.trim());
-        return taskList[taskNumber-1];
+        return tasks.get(taskNumber-1);
     }
 
-    public static void addToList(Task[] taskList, String userInput, String taskType) {
+    public static void addToList(String userInput, String taskType) {
         String replacedInput = userInput.replaceFirst("(?i)" + taskType, "").trim();
         try {
             switch (taskType) {
             case "todo":
-                addToDoToList(taskList, replacedInput);
+                addToDoToList(replacedInput);
                 break;
             case "deadline":
-                addDeadlineToList(taskList, replacedInput);
+                addDeadlineToList(replacedInput);
                 break;
             case "event":
-                addEventToList(taskList, replacedInput);
+                addEventToList(replacedInput);
                 break;
             }
         } catch (StringIndexOutOfBoundsException e) {
@@ -121,62 +146,57 @@ public class Duke {
         printDivider();
     }
 
-    public static void printAddedTask(Task[] taskList) {
+    public static void printAddedTask() {
         printDivider();
         System.out.println("\t" + OKAY_EMOJI + " Understood, sir. I've added this task: ");
-        System.out.println("\t\t" + taskList[listIndex]);
-        System.out.printf("\tYou currently have %d task(s) in your list, sir.%n", taskCount);
+        System.out.println("\t\t" + tasks.get(listIndex));
+        System.out.printf("\tYou currently have %d task(s) in your list, sir.%n", tasks.size());
         printDivider();
     }
 
-    public static void addToDoToList(Task[] taskList, String replacedInput) throws DukeException {
+    public static void addToDoToList(String replacedInput) throws DukeException {
         if (replacedInput.isBlank()) {
             throw new DukeException();
         }
-        taskList[listIndex] = new ToDo(replacedInput);
-        taskCount++;
-        printAddedTask(taskList);
+        tasks.add(new ToDo(replacedInput));
+        printAddedTask();
         listIndex++;
     }
 
-    public static void addDeadlineToList(Task[] taskList, String replacedInput) throws StringIndexOutOfBoundsException,
-            DukeException {
+    public static void addDeadlineToList(String replacedInput) throws StringIndexOutOfBoundsException, DukeException {
         if (replacedInput.isBlank()) {
             throw new DukeException();
         }
         int byIndex = replacedInput.toLowerCase().indexOf("/by");
         String by = replacedInput.substring(byIndex + 3);
         String description = replacedInput.substring(0, byIndex);
-        taskList[listIndex] = new Deadline(description, by);
-        taskCount++;
-        printAddedTask(taskList);
+        tasks.add(new Deadline(description, by));
+        printAddedTask();
         listIndex++;
     }
 
-    public static void addEventToList(Task[] taskList, String replacedInput) throws StringIndexOutOfBoundsException,
-            DukeException {
+    public static void addEventToList(String replacedInput) throws StringIndexOutOfBoundsException, DukeException {
         if (replacedInput.isBlank()) {
             throw new DukeException();
         }
         int atIndex = replacedInput.toLowerCase().indexOf("/at");
         String at = replacedInput.substring(atIndex + 3);
         String description = replacedInput.substring(0, atIndex);
-        taskList[listIndex] = new Event(description, at);
-        taskCount++;
-        printAddedTask(taskList);
+        tasks.add(new Event(description, at));
+        printAddedTask();
         listIndex++;
     }
 
 
-    public static void displayList(Task[] taskList) {
+    public static void displayList() {
         printDivider();
-        if (taskList[0] == null) {
+        if (tasks.isEmpty()) {
             System.out.println("\t" + HURRAY_EMOJI + " You have no tasks recorded, sir. Huzzah!");
         } else {
             System.out.println("\t" + LIST_EMOJI + " Here are your tasks, sir: ");
-            for (int i = 0; i < taskCount; i++) {
+            for (int i = 0; i < tasks.size(); i++) {
                 System.out.printf("\t%d.", i + 1);
-                System.out.println(taskList[i]);
+                System.out.println(tasks.get(i));
             }
         }
         printDivider();
@@ -199,7 +219,7 @@ public class Duke {
 
     public static void printGreeting() {
         printDivider();
-        System.out.println("\t" + WAVE_EMOJI + " Greetings, sir. My name is duke.Duke");
+        System.out.println("\t" + WAVE_EMOJI + " Greetings, sir. My name is Duke");
         System.out.println("\tHow may I assist you today, sir?");
         printDivider();
     }
